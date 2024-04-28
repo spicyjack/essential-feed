1f3729cc55d6a28f7c85b3a34c4fe976d7924f01
Brian Manning <brian@xaoc.org>
2024-04-27 23:26:09 -0700

    Project: added FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Brian Manning on 4/27/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
