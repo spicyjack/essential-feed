8ea1f8f9f4c7c3c4cffbd6c0cf10438db511e6da
Brian Manning <brian@xaoc.org>
2024-09-16 23:56:47 -0700

    HTTPClient: use an enum to represent `get()` results
    - Added an _HTTPClientResult_ enum to represent possible
      return values from the `get()` method (success/failure)
    - Updated `load()` to `switch` on the `result` of `get()`
    - Updated test cases with change in results from `get()`

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -70,18 +70,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         private var messages = [(url: URL,
-                                 completion: (Error?, HTTPURLResponse?) -> Void)]()
+                                 completion: (HTTPClientResult) -> Void)]()
 
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
         func get(from url: URL,
-                 completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+                 completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
 
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
 
         func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -89,8 +89,9 @@ final class RemoteFeedLoaderTests: XCTestCase {
                 url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
-                headerFields: nil)
-            messages[index].completion(nil, response)
+                headerFields: nil
+            )!
+            messages[index].completion(.success(response))
         }
     }
 }
